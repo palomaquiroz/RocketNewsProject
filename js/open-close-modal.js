b70f57c508adb3eee7b5d711b830fcbdecde64fa
Paloma Quiroz <paloma.quiroz@radiumrocket.com>
2021-10-22 11:20:59 -0300

    Deleted unused function

--- js/open-close-modal.js
+++ js/open-close-modal.js
@@ -7,11 +7,3 @@ subscriptionBtn.onclick = function () {
         close.onclick = function () {
             subscriptionModal.style.display = "none";
         }
-        function myFunction() {
-            var x = document.getElementById("links");
-            if (x.style.display === "block") {
-                x.style.display = "none";
-            } else {
-                x.style.display = "block";
-            }
-        }
